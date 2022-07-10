61f9e06563fec810aa3fd546332f4485ba3b6a14
pavi <pavicalderon49@gmail.com>
2022-07-10 17:11:37 -0600

    se añadio el menu

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">APP Medicos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?opcion=principal">Principal</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Altas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?opcion=alta_espe">Especialidad</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_habitacion">Habitacion</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_ingreso">Ingreso</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_medico">Medico</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_persona">Persona</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_paciente">Paciente</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_poblacion">Poblacion</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_provincia">Provincia</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mostrar
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="index.php?opcion=mostrar_espe">Especialidad</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_habitacion">Habitacion</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_ingreso">Ingreso</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_medico">Medico</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_persona">Persona</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_paciente">Paciente</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_poblacion">Poblacion</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_provincia">Provincia</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index2.php?opcion=login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
