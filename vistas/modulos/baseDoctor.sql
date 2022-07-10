a133fc45a0182925b1bbb80dbd70c318301ea294
pavi <pavicalderon49@gmail.com>
2022-07-10 00:29:22 -0600

    se creo el codigo de la primera tabla

---
+++ vistas/modulos/baseDoctor.sql
@@ -0,0 +1,8 @@
+CREATE TABLE persona
+(
+    codigo_persona INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL,
+    apellido1 VARCHAR(20) NOT NULL,
+    apellido2 VARCHAR(20) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
+)
