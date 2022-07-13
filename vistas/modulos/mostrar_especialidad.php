fb32fa31e6a5f6e318131f22aeacdbcfcc1c7f46
pavi <pavicalderon49@gmail.com>
2022-07-13 15:13:05 -0600

    se agregop contenido a mostrar especialidad

---
+++ vistas/modulos/mostrar_especialidad.php
@@ -0,0 +1,29 @@
+<?php 
+// Database connection info 
+$dbDetails = array( 
+'host' => 'localhost', 
+'user' => 'root', 
+'pass' => '', //Se deja en blanco porque en este caso el usuario root no tiene password 
+'db' => 'medicos'
+); 
+// mysql db table to use 
+$table = 'especialidad'; 
+// Table's primary key 
+$primaryKey = 'cod_especialidad'; 
+// Array of database columns which should be read and sent back to DataTables. 
+// The `db` parameter represents the column name in the database. 
+// The `dt` parameter represents the DataTables column identifier. 
+$columns = array
+( 
+    array( 'db' => 'cod_especialidad', 'dt' => 0 ),
+    array( 'db' => 'nombre_especialidad', 'dt' => 1 )
+); 
+// Include SQL query processing class 
+require 'ssp.class.php'; 
+// Output data as json format 
+echo json_encode( 
+SSP::simple($_GET, $dbDetails, $table, $primaryKey, $columns)
+);
+$especialidad = new Controlador();
+$especialidad -> MostrarEspecialidadControlador();
+?>
